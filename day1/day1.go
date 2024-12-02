987ca6aa55a41b987cb5cdc276baf35f8700afa8
Lynczera <leonardochen.code@gmail.com>
2024-12-02 11:37:29 -0700

    finish day1 part2

--- day1/day1.go
+++ day1/day1.go
@@ -9,6 +9,59 @@ import (
 	"strings"
 )
 
+func part1(list1 []int, list2 []int) {
+
+	result := 0
+
+	slices.Sort(list1)
+	slices.Sort(list2)
+
+	for i := 0; i < len(list1); i++ {
+		curr := list1[i] - list2[i]
+
+		if curr < 0 {
+			curr = curr * -1
+		}
+		result += curr
+
+	}
+
+	fmt.Println("%v", result)
+}
+
+func part2(list1 []int, list2 []int) {
+
+	slices.Sort(list1)
+	slices.Sort(list2)
+	result := 0
+
+	freq := make(map[int]int)
+
+	// calculate how many times a num in list1 appears in list2
+
+	for i := 0; i < len(list1); i++ {
+		_, ok := freq[list1[i]]
+		if !ok {
+			count := 0
+			for j := 0; j < len(list1); j++ {
+				if list2[j] == list1[i] {
+					count++
+				}
+
+			}
+			freq[list1[i]] = count
+		}
+
+	}
+
+	for k, v := range freq {
+
+		result += k * v
+	}
+	fmt.Println("%v", result)
+
+}
+
 func main() {
 	file, err := os.Open(os.Args[1])
 	if err != nil {
@@ -32,21 +85,7 @@ func main() {
 
 	}
 
-	result := 0
-
-	slices.Sort(list1)
-	slices.Sort(list2)
-
-	for i := 0; i < len(list1); i++ {
-		curr := list1[i] - list2[i]
-
-		if curr < 0 {
-			curr = curr * -1
-		}
-		result += curr
-
-	}
-
-	fmt.Println("%v", result)
+	// part1(list1, list2)
+	part2(list1, list2)
 
 }
