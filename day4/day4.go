6fd7798d8af446505c7472b8f2423ffaa5d1eb6f
Lynczera <leonardochen.code@gmail.com>
2024-12-17 19:14:24 -0300

    finish day5

--- day4/day4.go
+++ day4/day4.go
@@ -162,3 +162,167 @@ func graphIt(words []string) [][]string {
 	}
 	return graph
 }
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+// var directions = [][]int{[]int{1, 0}, []int{-1, 0}, []int{0, 1}, []int{0, -1}, []int{1, -1}, []int{1, 1}, []int{-1, -1}, []int{-1, 1}}
+
+const target = "XMAS"
+const revtarget = "SAMX"
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var words []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		words = append(words, line)
+
+	}
+	// println(part1(words))
+	println(part2(words))
+}
+func part1(words []string) int {
+	graph := graphIt(words)
+	count := 0
+
+	for r, _ := range graph {
+		rlimit := len(graph)
+		for c, _ := range graph[r] {
+			climit := len(graph[r])
+			//up
+			if r >= 3 {
+				w := ""
+				for i := range 4 {
+					w += graph[r+(-1*i)][c]
+				}
+				if w == target {
+					count++
+				}
+
+			}
+			//down
+			if r <= rlimit-4 {
+				w := ""
+				for i := range 4 {
+					w += graph[r+i][c]
+				}
+				if w == target {
+					count++
+				}
+			}
+			//left
+			if c >= 3 {
+				w := ""
+				for i := 0; i >= -3; i-- {
+					w += graph[r][c+i]
+				}
+				if w == target {
+					count++
+				}
+			}
+			//right
+			if c <= climit-4 {
+				w := ""
+				for i := range 4 {
+					w += graph[r][c+i]
+				}
+				if w == target {
+					count++
+				}
+			}
+			// ul
+			if r >= 3 && c >= 3 {
+				w := ""
+				for t := range 4 {
+					w += graph[r+(-1*t)][c+(-1*t)]
+				}
+				if w == target {
+					count++
+				}
+			}
+			// ur
+			if r >= 3 && c <= climit-4 {
+				w := ""
+				for t := range 4 {
+					w += graph[r+(-1*t)][c+t]
+				}
+				if w == target {
+					count++
+				}
+			}
+			// dl
+			if r <= rlimit-4 && c >= 3 {
+				w := ""
+				for t := range 4 {
+					w += graph[r+t][c+(-1*t)]
+				}
+				if w == target {
+					count++
+				}
+			}
+			// dr
+			if r <= rlimit-4 && c <= climit-4 {
+				w := ""
+				for t := range 4 {
+					w += graph[r+t][c+t]
+				}
+				if w == target {
+					count++
+				}
+			}
+
+		}
+
+	}
+	return count
+}
+
+func part2(words []string) int {
+	graph := graphIt(words)
+	count := 0
+
+	for r, _ := range graph {
+		rlimit := len(graph)
+		for c, _ := range graph[r] {
+			climit := len(graph[r])
+			if graph[r][c] == "A" {
+				//check bounds
+				if r >= 1 && r <= rlimit-2 && c >= 1 && c <= climit-2 {
+					if ((graph[r-1][c-1] == "M" && graph[r+1][c+1] == "S") || (graph[r-1][c-1] == "S" && graph[r+1][c+1] == "M")) && ((graph[r-1][c+1] == "S" && graph[r+1][c-1] == "M") || (graph[r-1][c+1] == "M" && graph[r+1][c-1] == "S")) {
+						count++
+					}
+
+				}
+			}
+		}
+
+	}
+	return count
+}
+
+func graphIt(words []string) [][]string {
+	graph := [][]string{}
+
+	for _, row := range words {
+		currRow := []string{}
+		for _, letter := range row {
+			currRow = append(currRow, string(letter))
+
+		}
+		graph = append(graph, currRow)
+	}
+	return graph
+}
--- day5/day5.go
+++ day5/day5.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bufio"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -33,7 +36,140 @@ func main() {
 		}
 
 	}
+
+	println(part1(rules, updates))
+	println(part2(rules, updates))
 }
-func part1(words []string) {
+func part1(rules []string, updates []string) int {
+	count := 0
+	rulesmap := buildRules(rules)
+
+	for _, u := range updates {
+		rulesarr := strings.Split(u, ",")
+		valid := true
+		for j := 0; j < len(rulesarr); j++ {
+			curr_rules, _ := rulesmap[rulesarr[j]]
+			for k := j + 1; k < len(rulesarr); k++ {
+				if slices.Contains(curr_rules, rulesarr[k]) {
+					valid = false
+					break
+
+				} else {
+					continue
+
+				}
+
+			}
+			if !valid {
+				break
+			}
+
+		}
+
+		if valid {
+			parsedVal, _ := strconv.Atoi(rulesarr[len(rulesarr)/2])
+			count += parsedVal
+		}
+
+	}
+	return count
+
+}
+
+func part2(rules []string, updates []string) int {
+	count := 0
+	countfixed := 0
+	rulesmap := buildRules(rules)
+
+	for _, u := range updates {
+		// curr_rules := rulesmap[u]
+		rulesarr := strings.Split(u, ",")
+		valid := true
+		for j := 0; j < len(rulesarr); j++ {
+			curr_rules, _ := rulesmap[rulesarr[j]]
+			for k := j + 1; k < len(rulesarr); k++ {
+				if slices.Contains(curr_rules, rulesarr[k]) {
+					valid = false
+					break
+
+				} else {
+					continue
+
+				}
+
+			}
+			if !valid {
+				break
+			}
+
+		}
+
+		if valid {
+			parsedVal, _ := strconv.Atoi(rulesarr[len(rulesarr)/2])
+			count += parsedVal
+		} else {
+			fixedUpdate := fixUpdate(rulesarr, rulesmap)
+			parsedfixed, _ := strconv.Atoi(fixedUpdate[len(fixedUpdate)/2])
+			countfixed += parsedfixed
+
+		}
+
+	}
+	return countfixed
+
+}
+
+// 5397,
+func fixUpdate(ups []string, rulesmap map[string][]string) []string {
+
+	// curr_rules := rulesmap[u]
+	reset := true
+	for reset {
+		valid := true
+		for j := 0; j < len(ups); j++ {
+			curr_rules, _ := rulesmap[ups[j]]
+			for k := j + 1; k < len(ups); k++ {
+				if slices.Contains(curr_rules, ups[k]) {
+					a := ups[j]
+					b := ups[k]
+					ups[j] = b
+					ups[k] = a
+					valid = false
+					break
+
+				} else {
+					continue
+
+				}
+
+			}
+
+		}
+		if valid {
+			reset = false
+		}
+	}
+	return ups
+
+}
+func buildRules(rules []string) map[string][]string {
+
+	res := make(map[string][]string)
+
+	//build a reverse rule -> 75 = [23, 24] -> all the numbers that 75 has to go after / all the numbers that must come before 75
+	for _, rule := range rules {
+		splitrule := strings.Split(rule, "|")
+		key := splitrule[1]
+		currRules, found := res[key]
+		if found {
+			currRules = append(currRules, splitrule[0])
+			res[key] = currRules
+
+		} else {
+			res[key] = []string{splitrule[0]}
+		}
+
+	}
+	return res
 
 }
